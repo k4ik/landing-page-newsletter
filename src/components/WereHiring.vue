3a75bfa2353ebfa63ca2df264d6cf8cdf9298526
k4ik <kaikleal1234@gmail.com>
2024-03-15 16:21:37 -0300

    feat: create We're hiring component

---
+++ src/components/WereHiring.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex bg-black max-w-sm text-xs text-zinc-100 p-2 rounded-3xl items-center gap-x-2 mb-8">
+        <span class="bg-cyan-600 rounded-3xl p-1 font-bold">Tecnologias</span>
+        <div class="flex items-center gap-x-4">
+            <span>Estamos por dentro dela para ajudar você</span>
+            <button class="text-base"><i class="fa-solid fa-arrow-right"></i></button>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
