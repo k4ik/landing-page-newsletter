f92abe23688789fb3f03cc5284fbb2ed598e8011
k4ik <kaikleal1234@gmail.com>
2024-03-15 15:40:25 -0300

    feat: create navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-
+  import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
-
+  <div class="bg-gray-900 h-screen">
+    <Navbar />
+  </div>
 </template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <header class="flex justify-between items-center py-8 px-5">
+        <div class="flex gap-14 items-center">
+            <img src="../assets/logo.svg" alt="tailwind logo">
+            <nav>
+                <ul class="flex gap-14">
+                    <li><a href="#" class="text-zinc-100 hover:text-cyan-500 transition">Produtos</a></li>
+                    <li><a href="#" class="text-zinc-100 hover:text-cyan-500 transition">Serviços</a></li>
+                    <li><a href="#" class="text-zinc-100 hover:text-cyan-500 transition">Planos</a></li>
+                    <li><a href="#" class="text-zinc-100 hover:text-cyan-500 transition">Contato</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="flex gap-14">
+            <button class="text-zinc-100 hover:text-cyan-500 transition">Entrar</button>
+            <button class="bg-cyan-500 text-zinc-100 font-normal transition hover:bg-zinc-400 py-3 px-6">Cadastrar</button>
+        </div>
+    </header>
+</template>
+
+<script setup lang="ts">
+
+</script>
