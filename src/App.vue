1fc1888cad135985dfe4f6b59214df176db434ea
k4ik <kaikleal1234@gmail.com>
2024-03-23 19:20:07 -0300

    feat: structure overlay

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
   import Navbar from "./components/Navbar.vue";
   import Main from "./components/Main.vue";
+  import Overlay from "./components/Overlay.vue";
 </script>
 
 <template>
   <div class="bg-gray-900 h-screen">
+    <Overlay />
     <Navbar />
     <Main />
   </div>
---
+++ src/components/Overlay.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div>
+            <div>
+                <h1>Cadastre-se Agora!!!</h1>
+                <button>X</button>
+            </div>
+            <p>Complete o cadastro para participar da newsletter e acessar as informações da newsletter</p>
+        </div>
+        <form>
+            <label for="">Nome</label>
+            <input type="text" placeholder="Digite seu nome aqui">
+
+            <label for="">Email</label>
+            <input type="text" placeholder="Digite seu melhor email">
+
+            <input type="checkbox" name="" id="">
+            <span>Eu aceito os Termos de serviço para acessar as informações da newsletter</span>
+
+            <button>Cadastrar na nossa newsletter</button>
+        </form>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
