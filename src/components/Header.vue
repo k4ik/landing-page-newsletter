9b883faa06f4e8408f3095eda819fa677d901016
k4ik <kaikleal1234@gmail.com>
2024-03-30 21:40:36 -0300

    feat: create header component

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <header>
+        <img src="../assets/images/logo.svg" alt="tailwind logo">
+        <button>Cadastrar</button>
+    </header>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import '../assets/scss/header';
+</style>
