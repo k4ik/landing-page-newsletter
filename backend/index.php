13c395ad1e60560711c001adcc30be76e9613726
k4ik <kaikleal1234@gmail.com>
2025-01-09 11:18:26 -0300

    feat: add new routes

--- backend/index.php
+++ backend/index.php
@@ -1,10 +1,12 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: POST, GET, DELETE");
 header("Access-Control-Allow-Headers: Content-Type");
 
 use Controller\AuthController;
 use Controller\PostController;
+use Controller\MembersController;
+use Controller\OwnersController;
 
 require './vendor/autoload.php';
 require './includes/con.php';
@@ -12,27 +14,65 @@ require './includes/con.php';
 $method = $_SERVER['REQUEST_METHOD'];
 $path = $_SERVER['REQUEST_URI'];
 
-$auth = new AuthController($con);
-$post = new PostController($con);
+$authController = new AuthController($con);
+$postController = new PostController($con);
+$membersController = new MembersController($con);
+$ownersController = new OwnersController($con);
 
-if ($method == 'POST' && $path == '/signup') {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
+switch($method) {
+    case 'POST':
+        if($path == '/api/signup') {
+            $name = $_POST["name"];
+            $email = $_POST["email"];
+        
+            $authController->signup($name, $email);
+        }
 
-    $auth->signup($name, $email);
-} elseif ($method == 'POST' && $path == '/login') {
-    $email = $_POST["email"];
-    $password = $_POST["password"];
+        if($path == '/api/login') {
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+        
+            $authController->login($email, $password);
+        }
 
-    $auth->login($email, $password);
-} elseif ($method == 'POST' && $path == '/post') {
-    $title = $_POST["title"];
-    $content = $_POST["content"];
+        if($path == '/api/post') {
+            $title = $_POST["title"];
+            $content = $_POST["content"];
+        
+            $postController->createPost($title, $content);
+        }
 
-    $post->createPost($title, $content);
-} else {
-    http_response_code(404);
-    echo "404 Not Found";
+        if($path == '/api/owner') {
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+
+            $ownersController->addOwner($email, $password);
+        }
+        break;
+    case 'GET':
+        if($path == '/api/members') {
+            $membersController->getNewsletterMembers();
+        }
+
+        if($path == '/api/owners') {
+            $ownersController->getNewsletterOwners();
+        }
+
+        if($path == '/api/posts') {
+            $postController-> getPosts();
+        }
+        break;
+    case 'DELETE':
+        if($path == '/api/owner/'.$id) {
+            $ownersController->deleteOwner($id);
+        }
+
+        if($path == '/api/member/'.$id) {
+            $membersController->deleteMember($id);
+        }
+    default:
+        http_response_code(404);
+        echo "404 Not Found";
 }
 
 pg_close($con);
