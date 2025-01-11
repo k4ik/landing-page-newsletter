396262a10ece18c79139a7b65c02a22488ef7f87
k4ik <kaikleal1234@gmail.com>
2025-01-11 17:12:54 -0300

    fix: routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,44 +1,44 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: () => import('../views/LoginView.vue')
-    },
-    {
-      path: '/painel',
-      name: 'painel',
-      component: () => import('../views/PainelView.vue'),
-      meta: { requiresAuth: true } 
-    },
-    {
-      path: '/create-post',
-      name: 'newsletter',
-      component: () => import('../views/NewsletterView.vue'),
-      meta: { requiresAuth: true } 
-    }
-  ]
-})
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView,
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: () => import('../views/LoginView.vue'),
+        },
+        {
+            path: '/panel',
+            name: 'panel',
+            component: () => import('../views/PanelView.vue'),
+            meta: { requiresAuth: true }
+        },
+        {
+            path: '/create-post',
+            name: 'newsletter',
+            component: () => import('../views/NewsletterView.vue'),
+            meta: { requiresAuth: true }
+        },
+    ],
+});
 
 router.beforeEach((to, from, next) => {
-  if (to.meta.requiresAuth && !isLoggedIn()) {
-    next('/login');
-  } else {
-    next();
-  }
+    if (to.meta.requiresAuth && !isLoggedIn()) {
+        next('/login');
+    } else {
+        next();
+    }
 });
 
 function isLoggedIn() {
-  return localStorage.getItem('token');
+    return localStorage.getItem('token');
 }
 
-export default router
+export default router;
